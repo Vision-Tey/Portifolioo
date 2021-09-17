ac5a06107f29f0b28e24fc055bc014c2ea1ea869
Vision-Tey <nimusiimavision@gmail.com>
2021-09-17 11:34:33 +0300

    -Add index.html -Add style.css -Add script.js -Add backg.jpg

---
+++ script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scrolly > 20){
+
+//         }else
+//     })
+// });
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+    
+});
